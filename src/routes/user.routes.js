ba2b894c14ac4aee6c044191b3bf21f4a1fc9e6d
Aryamanshu Mishra <93377648+Aryamanshu@users.noreply.github.com>
2024-09-04 18:45:03 +0530

    added all routes in user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registeruser, refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    loginUser, 
+    logoutUser, 
+    registeruser, 
+    refreshAccessToken, 
+    changeCurrentpassword, 
+    getCurrentUser, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    updateAccountDetails 
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,6 +36,13 @@ router.route("/register").post( // fields me bhut sare options hote hai abhi bs 
 //secured Routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentpassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)  //patch rkhna zarrori hai agar post rakh diya toh sari details update hojengy account k sath
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)  //patch sirf ek avtar update ho rha hai
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage) // upload isliye kr rhe h kyu ki multer middleware h bhi hai
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)  ///c/: k baad dena padega username kyu ki ye params se arha hai
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 
 
