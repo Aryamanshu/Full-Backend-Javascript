478a7b99d38027778736aa77290efb4c717388a2
Aryamanshu Mishra <93377648+Aryamanshu@users.noreply.github.com>
2024-09-04 12:11:57 +0530

    done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -447,6 +447,12 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
     if(!channel?.length) {
         throw new ApiError(404, "channel does not exists")
     }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, "User channel fetched successfully")
+    )
 })
 
 
