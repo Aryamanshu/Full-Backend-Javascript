93e288a0511ccb0687ce276b6da0ea719fb369e1
Aryamanshu Mishra <93377648+Aryamanshu@users.noreply.github.com>
2024-09-05 20:52:16 +0530

    updated comment, healthcheck, like, subscription controllers

--- src/routes/healthcheck.routes.js
+++ src/routes/healthcheck.routes.js
@@ -3,6 +3,6 @@ import { healthcheck } from "../controllers/healthcheck.controller.js"
 
 const router = Router();
 
-router.route('/').get(healthcheck);
+router.route('/healthCheck').get(healthcheck);
 
 export default router
